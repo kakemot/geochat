b5f869f15240de67661027dffff95d80a334c02a
Tony Arntsen <tonyarntsen@Tonys-Air.home>
2024-02-25 11:29:18 +0100

    Remove port specification

--- pages/index.vue
+++ pages/index.vue
@@ -30,7 +30,7 @@ let socket: any = null;
 
 onMounted(() => {
   let wsProtocol = window.location.protocol === "https:" ? "wss:" : "wss:";
-  socket = new WebSocket(`${wsProtocol}//geochat-bridge-quqoh4a5iq-ew.a.run.app:8080`);
+  socket = new WebSocket(`wss://geochat-bridge-quqoh4a5iq-ew.a.run.app`);
   socket.onopen = function(event: any) {
     console.log('Connected to WebSocket');
   };
